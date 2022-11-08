000e0ad6214bc2c208e34366d30285a2f40b188c
Tucker Johnson <john5653@stthomas.edu>
2022-11-07 22:37:30 -0600

    Updated Navigation

--- server.js
+++ server.js
@@ -38,96 +38,146 @@ app.get('/', (req, res) => {
 // Dynamic File path for homepage --> index.html
 
 app.get('/homepage', (req, res) => {
-    // 1. Still need to add error handle
-    // 2. If there is any data from database add here
+    // Retrieve homepage template
     fs.readFile(path.join(template_dir, 'index.html'), (err, template) => {
-        let query = 'SELECT Year FROM AnnualSectorEnergy';
-        db.all(query, [], (err, rows) => {
-            // have an embedded one for state name population
-            let response = template.toString(); 
-            let years = ""; 
-            for (let i = 0; i < rows.length; i++) {
-                years = years + '<li><a href="/year/'+rows[i].Year+'" target="_self">'+ rows[i].Year + '</a></li>';
-            }
-            response = response.replace('%%List_Placeholder_Total_Year%%', years);
-            res.status(200).type('html').send(response);
-        });
-         
-    });
-});
-
-app.get('/sector/:selected_sector', (req, res) => {
-    console.log('request 2');
-    // console.log(req.params.selected_sector);
-    let annual_query ="";
-    let month_query = ""; // with month query need to join with month table
-    if (req.params.selected_sector == "Residential") {
-        annual_query = 'SELECT ID, Year, Geothermal_Res, Solar_Res, \
-        Wood_Res, Total_Consumed_Res  FROM sector_annual_data';
-        month_query = 'SELECT ID, Year, Month, Geothermal_Res, Solar_Res, \
-        Wood_Res, Total_Consumed_Res  FROM sector_month_data';
-    } else if (req.params.selected_sector == "Industrial") {
-        annual_query = 'SELECT ID, Year, Hydroelectric_Ind, Geothermal_Ind, Solar_Ind, \
-        Wind_Ind, Wood_Ind, Waste_Ind, Fuel_Ethanol_Ind, \
-        Biomass_Total_Ind, Total_Consumed_Ind FROM sector_annual_data';
-        month_query = 'SELECT ID, Year, Month, Hydroelectric_Ind, Geothermal_Ind, Solar_Ind, \
-        Wind_Ind, Wood_Ind, Waste_Ind, Fuel_Ethanol_Ind, \
-        Biomass_Total_Ind, Total_Consumed_Ind FROM sector_month_data';
-    } else if (req.params.selected_sector == "Commercial") {
-        annual_query = 'SELECT ID, Year, Month, Hydroelectric_Comm, Geothermal_Comm, Solar_Comm, \
-        Wind_Comm, Wood_Comm, Waste_Comm, Fuel_Ethanol_Comm, \
-        Biomass_Total_Comm, Total_Consumed_Comm FROM sector_annual_data';
-        month_query = 'SELECT ID, Year, Month, Hydroelectric_Comm, Geothermal_Comm, Solar_Comm, \
-        Wind_Comm, Wood_Comm, Waste_Comm, Fuel_Ethanol_Comm, \
-        Biomass_Total_Comm, Total_Consumed_Comm FROM sector_month_data';
-    } else if (req.params.selected_sector == "Transportation") {
-        annual_query = 'SELECT ID, Year, Biodisel_Trans, Renew_Diesel_Trans, \
-        Other_Biofuels_Trans, Total_Consumed_Trans FROM sector_annual_data';
-        month_query = 'SELECT ID, Year, Month, Biodisel_Trans, Renew_Diesel_Trans, \
-        Other_Biofuels_Trans, Total_Consumed_Trans FROM sector_month_data';
-    } else if (req.params.selected_sector == "Electric") {
-         annual_query = 'SELECT ID, Year, Hydroelectric_Electric, Geothermal_Electric, \
-         Solar_Electric, Wind_Electric, Wood_Electric, Waste_Electric, \
-         Biomass_Total_Electric, Total_Consumed_Electric FROM sector_annual_data';
-         month_query = 'SELECT ID, Year, Month, Hydroelectric_Electric, Geothermal_Electric, \
-         Solar_Electric, Wind_Electric, Wood_Electric, Waste_Electric, \
-         Biomass_Total_Electric, Total_Consumed_Electric FROM sector_month_data';
-    } else {
-        let string = '<h1>File Not Found<h1>';
-        res.status(404).type('html').send(string);
-        return;
-    }
-    fs.readFile(path.join(template_dir, 'sector.html'), (err, data) => {
         if (err) {
-            res.status(404).type('html').send(err);
-            // send error client -- 404
+            // Retrieve error client notice template
+            fs.readFile(path.join(template_dir, 'file_not_found.html'), (err, template) => {
+                if (err) {
+                    // In case client notice template cannot be accessed
+                    res.status(404).type('text').send('Please check your request and try again...')
+                    return
+                }
+                res.status(404).type('html').send(template)
+                return
+            })
         }
-        db.all(annual_query,[], (err, annual_data) => {
-            console.log(err, annual_data);
+        let query = 'SELECT Year FROM AnnualSectorEnergy WHERE sector_id=1;'
+        db.all(query, [], (err, rows) => {
+            // If database error occurs
+            if (err) {
+                // Retrieve error client notice template
+                fs.readFile(path.join(template_dir, 'file_not_found.html'), (err, template) => {
+                    if (err) {
+                        // In case client notice template cannot be accessed
+                        res.status(404).type('text').send('Please check your request and try again...')
+                    }
+                    res.status(404).type('html').send(template)
+                    return
+                })
+                res.status(404).type('html').send(template)
+                return
+            }
+            // Populate Client Navigation
+            // Navigation for Total Annual
+            let response = template.toString() 
+            let total_years = ""
+            let sector_years = ""
+            for (let i = 0; i < rows.length; i++) {
+                total_years = total_years + `<li><a href="/total/annual/${rows[i].year}" \
+                target="_self">${rows[i].year}</a></li>`
+                sector_years = sector_years + `<li><a href="/sector/annual/${rows[i].year}" \
+                target="_self">${rows[i].year}</a></li>`
+            }
+            // Navigation for Total and Sector Monthly
+            let total_months = ""
+            let sector_months = ""
+            let months_array = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July',
+                                'August', 'September', 'October', 'November', 'December']
+            let ul_head_tag = `<ul class="menu" style="max-height: 300px; overflow-y:scroll;">`  
+            let ul_li_end_tag = `</ul></li> <div class="sidebar_buffer"></div>`          
+            for (let i = 0; i < 12; i++) {
+                let li_head_tag = `<li><a>${months_array[i]}</a>`
+                total_months = total_months + li_head_tag + ul_head_tag
+                sector_months = sector_months + li_head_tag + ul_head_tag 
+                for (let x  = 0; x < rows.length; x++) {
+                    total_months = total_months + `<li><a href="/total/monthly/${months_array[i]}/${rows[x].year}" \
+                    target="_self">${rows[x].year}</a></li>`
+                    sector_months = sector_months + `<li><a href="/sector/monthly/${months_array[i]}/${rows[x].year}" \
+                    target="_self">${rows[x].year}</a></li>`
+                }
+                
+                total_months = total_months + ul_li_end_tag
+                sector_months = sector_months + ul_li_end_tag
+            }
+            // Navigation for State
+            let state_array = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California'
+            , 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii'
+            , 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana'
+            , 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi'
+            , 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey'
+            , 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma'
+            , 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota'
+            , 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington'
+            , 'West Virginia', 'Wisconsin', 'Wyoming']
+            let state = ""
+            for (let i = 0; i < state_array.length; i++) {
+                state = state + `<li><a href="/state/${state_array[i]}" \
+                target="_self">${state_array[i]}</a></li>`
+            }
 
-            db.all(month_query, [], (err, monthly_data ) => {
-                console.log(err, monthly_data);
+            // queries to fill homepage with overview diagrams and tables will be executed here
 
-                // here is where I will add the data to the javascript representations-- 
-                // how to do this????
-            });
-        });
+            response = response.replace('%%List_Placeholder_Total_Year%%', total_years)
+            response = response.replace('%%List_Placeholder_Sector_Annual%%', sector_years)
+            response = response.replace('%%List_Placeholder_Total_Month%%', total_months)
+            response = response.replace('%%List_Placeholder_Sector_Monthly%%', sector_months)
+            response = response.replace('%%List_Placeholder_State%%', state)
+            res.status(200).type('html').send(response)
+        })
+    })
+})
 
-        // Convert Template from Binary to String
-        let response = data.toString();
-        // Replace necessary placeholders in HTML Template
-        response = response.replace('%%Sector_Type%%', req.params.selected_sector);
-        // Retrieve data from database
+// Dynamic path for Sector Annual Data
 
-        res.status(200).type('html').send(response);
-    });
-});
+app.get('/:sector/annual/:year', (req, res) => {
+    let sector = req.params.sector
+    let year = req.params.year
+
+    // todo add html template
+    fs.readFile(path.join(template_dir, 'file_not_found.html'), 'utf-8', (err, template) => {
+        // todo write query
+        let query = 
+            ``
+            
+        db.all(query, [], (err, rows) => {
+            let response = template.toString()
+            // todo replace placeholders
+            
+            res.status(200).type('html').send(response)
+        })
+    })
+})
+
+// Dynamic path for Sector Monthly Data
+
+app.get('/:sector/monthly/:month/:year', (req, res) => {
+    let sector = req.params.sector
+    let year = req.params.year
+    let month = req.params.month
+
+    // todo add html template
+    fs.readFile(path.join(template_dir, 'file_not_found.html'), 'utf-8', (err, template) => {
+        // todo write query
+        let query = 
+            ``
+            
+        db.all(query, [], (err, rows) => {
+            let response = template.toString()
+            // todo replace placeholders
+            
+            res.status(200).type('html').send(response)
+        })
+    })
+})
+
+// Dynamic path for State Data
 
 app.get('/state/:state', (req, res) => {
     let state = req.params.state
 
     // todo add html template
-    fs.readFile(file_not_found.html, 'utf-8', (err, template) => {
+    fs.readFile(path.join(template_dir, 'file_not_found.html'), 'utf-8', (err, template) => {
         // todo write query
         let query = 
             ``
@@ -140,12 +190,14 @@ app.get('/state/:state', (req, res) => {
         })
     })
 })
+
+// Dynamic path for Total Annual Data
 
 app.get('/total/annual/:year', (req, res) => {
     let year = req.params.year
 
     // todo add html template
-    fs.readFile(file_not_found.html, 'utf-8', (err, template) => {
+    fs.readFile(path.join(template_dir, 'file_not_found.html'), 'utf-8', (err, template) => {
         // todo write query
         let query = 
             ``
@@ -158,13 +210,15 @@ app.get('/total/annual/:year', (req, res) => {
         })
     })
 })
+
+// Dynamic path for Total Monthly Data
 
 app.get('/total/monthly/:month_id/:year', (req, res) => {
     let monthID = req.params.month_id
     let year = req.params.year
 
     // todo add html template
-    fs.readFile(file_not_found.html, 'utf-8', (err, template) => {
+    fs.readFile(path.join(template_dir, 'file_not_found.html'), 'utf-8', (err, template) => {
         // todo write query
         let query = 
             ``
